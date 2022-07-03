733c0fb128fb9deeb9abbbab1878b98018a48343
Dhia'a Shalabi <dhiaa.shalabi@gmail.com>
2022-07-03 17:55:53 +0300

    Add demo C# classes.

---
+++ CSharpClasses/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharpClasses
+{
+    class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "John";
+            person.Introduce("Dhia'a");
+            
+            var p = Person.Parse("Dhia'a");
+            p.Introduce("Ahmed");
+        }
+    }
+}
