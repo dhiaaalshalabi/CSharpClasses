8439c10033cce05896d1173f1d9312cd46802c9d
Dhia'a Shalabi <dhiaa.shalabi@gmail.com>
2022-07-04 04:17:41 +0300

    Updated program file with object initializers.

--- CSharpClasses/Program.cs
+++ CSharpClasses/Program.cs
@@ -1,6 +1,13 @@
 ﻿using CSharpClasses;
 
 var customer = new Customer();
+
+var customer1 = new Customer
+{
+    Name = "Customer Name",
+    Id = 4
+};
+
 var order = new Order();
 customer.Orders = new List<Order> { order };
 Console.WriteLine(customer.Id);
